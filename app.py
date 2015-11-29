d0206f0f6d8b6542fb204f26bdea319c443584fb
Elena <beklenischeva.elena@gmail.com>
2015-11-29 20:24:31 +0500

    bottle

--- app.py
+++ app.py
@@ -1,18 +1,81 @@
-from flask import Flask, send_file, render_template
+from bottle import route, static_file, redirect, run, load_app, Bottle
 
-app = Flask(__name__)
+app = application = Bottle()
 
 
-@app.route("/")
+class StripPathMiddleware(object):
+    def __init__(self, a):
+        self.a = a
+
+    def __call__(self, e, h):
+        e['PATH_INFO'] = e['PATH_INFO'].rstrip('/')
+        return self.a(e, h)
+
+
+@route("/")
 def index():
-    return send_file('index.html')
+    print('there1')
+    # print(request.environ["REMOTE_ADDR"])
+    # read_xml(request.environ["REMOTE_ADDR"])
+    redirect('index.html')
+
+
+@route("/index.html")
+def index_r():
+    return static_file('index.html', "./")
+
+
+@route("/hw.html")
+def hw():
+    return static_file('hw.html', "./")
+
+
+@route("/galery.html")
+def gallery():
+    return static_file('galery.html', "./")
+
+
+@route("/contacts.html")
+def contacts():
+    return static_file('contacts.html', "./")
+
+
+@route("/styles/<filename>")
+def styles(filename):
+    return static_file(filename, './styles/')
+
+
+@route("/css/<filename>")
+def css(filename):
+    return static_file(filename, './css/')
+
+
+@route('/favicon.ico')
+def favicon():
+    return static_file('favicon.ico', root='./')
+
+
+@route("/images/<filename>")
+def styles(filename):
+    return static_file(filename, './images/', mimetype='image/jpg')
+
+
+@route("/js/<filename>")
+def get_script(filename):
+    return static_file(filename, root='./js/')
+
+
+@route("/hw/<filename:path>")
+def hw_files(filename):
+    print(filename)
+    return static_file(filename, root="./hw")
+
 
 if __name__ == "__main__":
-    @app.route('/<path:filename>')
-    def serve_static(filename):
-        print(filename)
-        return send_file(filename)
-
-    app.run(debug=True, port=4000)
+    run(app=StripPathMiddleware(app),
+        host='0.0.0.0',
+        port=4000,
+        reloader=True)
+    # run(debug=True, port=4000, reloader=True)
 
 __author__ = 'Lena'
