6d3cfac615df142ace54a15538d1bf04faca974a
Elena <beklenischeva.elena@gmail.com>
2015-11-29 21:29:24 +0500

    no message

--- app.py
+++ app.py
@@ -12,63 +12,63 @@ class StripPathMiddleware(object):
         return self.a(e, h)
 
 
-@route("/")
-def index():
-    print('there1')
-    # print(request.environ["REMOTE_ADDR"])
-    # read_xml(request.environ["REMOTE_ADDR"])
-    redirect('index.html')
-
-
-@route("/index.html")
-def index_r():
-    return static_file('index.html', "./")
-
-
-@route("/hw.html")
-def hw():
-    return static_file('hw.html', "./")
-
-
-@route("/galery.html")
-def gallery():
-    return static_file('galery.html', "./")
-
-
-@route("/contacts.html")
-def contacts():
-    return static_file('contacts.html', "./")
-
-
-@route("/styles/<filename>")
-def styles(filename):
-    return static_file(filename, './styles/')
-
-
-@route("/css/<filename>")
-def css(filename):
-    return static_file(filename, './css/')
-
-
-@route('/favicon.ico')
-def favicon():
-    return static_file('favicon.ico', root='./')
-
-
-@route("/images/<filename>")
-def styles(filename):
-    return static_file(filename, './images/', mimetype='image/jpg')
-
-
-@route("/js/<filename>")
-def get_script(filename):
-    return static_file(filename, root='./js/')
-
-
-@route("/hw/<filename:path>")
-def hw_files(filename):
-    print(filename)
-    return static_file(filename, root="./hw")
+# @route("/")
+# def index():
+#     print('there1')
+#     # print(request.environ["REMOTE_ADDR"])
+#     # read_xml(request.environ["REMOTE_ADDR"])
+#     redirect('index.html')
+#
+#
+# @route("/index.html")
+# def index_r():
+#     return static_file('index.html', "./")
+#
+#
+# @route("/hw.html")
+# def hw():
+#     return static_file('hw.html', "./")
+#
+#
+# @route("/galery.html")
+# def gallery():
+#     return static_file('galery.html', "./")
+#
+#
+# @route("/contacts.html")
+# def contacts():
+#     return static_file('contacts.html', "./")
+#
+#
+# @route("/styles/<filename>")
+# def styles(filename):
+#     return static_file(filename, './styles/')
+#
+#
+# @route("/css/<filename>")
+# def css(filename):
+#     return static_file(filename, './css/')
+#
+#
+# @route('/favicon.ico')
+# def favicon():
+#     return static_file('favicon.ico', root='./')
+#
+#
+# @route("/images/<filename>")
+# def styles(filename):
+#     return static_file(filename, './images/', mimetype='image/jpg')
+#
+#
+# @route("/js/<filename>")
+# def get_script(filename):
+#     return static_file(filename, root='./js/')
+#
+#
+# @route("/hw/<filename:path>")
+# def hw_files(filename):
+#     print(filename)
+#     return static_file(filename, root="./hw")
 
 
 if __name__ == "__main__":
@@ -78,4 +78,4 @@ if __name__ == "__main__":
         reloader=True)
     # run(debug=True, port=4000, reloader=True)
 
-__author__ = 'Lena'
+
